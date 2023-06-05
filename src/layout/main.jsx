b0120edb33edb8da67137e50184b1e019d8846b2
budaaaeva <96288951+budaaaeva@users.noreply.github.com>
2023-06-05 14:37:15 +0300

    Update main.jsx

--- src/layout/main.jsx
+++ src/layout/main.jsx
@@ -9,14 +9,14 @@ class Main extends React.Component {
         loading: true,
     };
     componentDidMount() {
-        fetch("http://www.omdbapi.com/?apikey=154c7c3b&s=murder")
+        fetch("https://www.omdbapi.com/?apikey=154c7c3b&s=murder")
             .then((response) => response.json())
             .then((data) => this.setState({ movies: data.Search, loading: false }));
     }
 
     searchMovies = (str, type = "all") => {
         this.setState({ loading: true });
-        fetch("http://www.omdbapi.com/?apikey=154c7c3b&s=" + str + (type !== "all" ? '&type=' + type : ""))
+        fetch("https://www.omdbapi.com/?apikey=154c7c3b&s=" + str + (type !== "all" ? '&type=' + type : ""))
             .then((response) => response.json())
             .then((data) => this.setState({ movies: data.Search, loading: false }));
     };
